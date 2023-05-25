5b601ed9c24940f5d7ba80f215334d0a31808c1d
trisna-az <trisnaabdul.das@gmail.com>
2023-05-25 22:23:30 +0700

    add status schema

--- app/models/user.model.js
+++ app/models/user.model.js
@@ -3,11 +3,26 @@ module.exports = mongoose => {
     const schema = mongoose.Schema(
         {
             name: {type: String, unique: true},
-            phone: {type: String, unique: true}
+            phone: {type: String, unique: true},
+            status: {type: String, default: "new registered"}
         }, {
             timestamps: true
         }
     );
+    
+    schema.pre('save', function(next) {
+        const self = this;
+        mongoose.models["User"].findOne({$or:[{name : self.name}, {phone : self.phone}]},function(err, user) {
+            if(err) {
+                next(err);
+            } else if(user) {
+                self.status = "already registered";
+                next();
+            } else {
+                next();
+            }
+        });
+    });
 
     schema.method("toJSON", function(){
         const {__v, _id, ...object} = this.toObject();
